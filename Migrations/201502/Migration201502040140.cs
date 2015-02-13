bbc0473d20b727b58bf404d832011798a905becc
Lucian <lucian.mbraz@gmail.com>
2015-02-13 16:03:33 -0200

    alterei a estrutura de pastas

---
+++ Migrations/201502/Migration201502040140.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Migrations._201502
+{
+    [Migration(201502040140, "Criação das tabelas para uma primeira versão do banco de dados")]
+    public class Migration201502040140 : Migration
+    {
+        public override void Down()
+        {
+            
+        }
+
+        public override void Up()
+        {
+            
+        }
+    }
+}
---
+++ Storage/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage
+{
+    public interface IRepository<TEntity>
+    {
+        IQueryable<TEntity> Items { get; }
+
+        void Add(TEntity entity);
+
+        void Add(ICollection<TEntity> entities);
+
+        void Remove(TEntity entity);
+
+        void Remove(ICollection<TEntity> entities);
+
+        void Update(TEntity entity);
+
+        void Update(ICollection<TEntity> entities);
+
+        TEntity Get(object id); 
+    }
+}
